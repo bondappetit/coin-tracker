27fb1a1264e87344968a67cd5a18822687b630c6
frimuchkov <frimuchkov@yandex-team.ru>
2021-04-27 23:24:04 +0300

    Fix polling interval

--- src/manager.ts
+++ src/manager.ts
@@ -1,6 +1,6 @@
 import { fetchInfo as fetchUniswapInfo } from './trackers/uniswap';
 import { fetchInfo as fetchcoingeckoInfo } from './trackers/coingecko';
-import { fetchInfo as fetchetherscanInfo } from './trackers/ethplorer';
+import { fetchInfo as fetchethplorerInfo } from './trackers/ethplorer';
 import { sendHourlyInfo } from './telegram/tg';
 import * as config from 'config';
 import { COINS } from './coins';
@@ -8,7 +8,7 @@ import { COINS } from './coins';
 const coinName = config.get<string>('coin');
 const coin = COINS[coinName];
 
-const CYCLE_INTERVAL = 30000;
+const POLLING_INTERVAL = Number(config.get<string>('pollingInterval'));
 
 const ONE_HOUR = 1000 * 60 * 60;
 
@@ -36,10 +36,10 @@ export const startManager = async () => {
         }
 
         try {
-            await fetchetherscanInfo(coin.contract);
-            console.log('Info from EtherScan has been fetched');
+            await fetchethplorerInfo(coin.contract);
+            console.log('Info from Ethplorer has been fetched');
         } catch (e) {
-            console.error('Error while fetch info from EtherScan', e);
+            console.error('Error while fetch info from Ethplorer', e);
         }
 
         if (Date.now() >= nextTimeToSend) {
@@ -52,7 +52,7 @@ export const startManager = async () => {
             }
         }
         console.log('Iteration has been finished');
-        setTimeout(runCycle, CYCLE_INTERVAL);
+        setTimeout(runCycle, POLLING_INTERVAL);
     };
     await runCycle();
 };
